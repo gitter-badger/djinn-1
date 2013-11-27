0d94ac8772b75bc5249b39b6eaf019f559c0a980
Ashleigh Pook <ashleighpook@gmail.com>
2013-11-27 08:52:46 +0000

    Added a Commands Action.

---
+++ source/Sungiant.Djinn.Specification/Source/Actions/Commands.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Sungiant.Djinn.Specification
+{
+	public class Commands
+		: IAction
+	{
+		// Base class data
+		public String Type { get; set; }
+		public String Description { get; set; }
+
+		// CommandSpecification data
+		public Boolean IsContextRemote { get; set; }
+		public String[] Values { get; set; }
+		public Boolean IgnoreFailure { get; set; }
+	}
+}
+
---
+++ source/Sungiant.Djinn/Source/Actions/Commands.cs
@@ -0,0 +1,47 @@
+using System;
+using Sungiant.Cloud;
+using Sungiant.Core;
+
+namespace Sungiant.Djinn
+{
+	public class Commands
+		: Action<Specification.Commands>
+	{
+		public Commands (Specification.Commands specification, String djinnContext) 
+			: base (specification, djinnContext) {}
+
+		MachineContext Context
+		{ 
+			get { return Specification.IsContextRemote ? MachineContext.Remote : MachineContext.Local; }
+		}
+
+		public override void Perform (ICloudProvider cloudProvider, ICloudDeployment cloudDeployment)
+		{
+			LogPerform ();
+
+			foreach (String val in Specification.Values)
+			{
+				switch (Context)
+				{
+					case MachineContext.Local: 
+						{
+							// todo: this should always be run from the DjinnContext
+							Int32 exitCode = ProcessHelper.Run (val, Console.WriteLine);
+
+							if (exitCode != 0 && !Specification.IgnoreFailure)
+							{
+								throw new Exception ("Exited with code " + exitCode);
+							}
+						}
+						break;
+					case MachineContext.Remote: 
+						{
+							cloudProvider.RunCommand (cloudDeployment, val, Specification.IgnoreFailure); 
+						}
+						break;
+				}
+			}
+		}
+	}
+}
+
