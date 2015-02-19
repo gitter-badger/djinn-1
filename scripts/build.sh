f4bac294314002e486ae7c8ed447b59b79667446
Ash Pook <ashleighpook@gmail.com>
2015-02-19 16:30:47 +0000

    I think this will make the build pass again. :D

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,13 @@
 
 startPath=`pwd`
 
+cd "source/"
+
 # tabs to spaces
 find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
 
 # run build
-xbuild "source/Sungiant.Djinn/Sungiant.Djinn.csproj" /p:Configuration=Release
+xbuild "Sungiant.Djinn/Sungiant.Djinn.csproj" /p:Configuration=Release
 
 cd $startPath
 
